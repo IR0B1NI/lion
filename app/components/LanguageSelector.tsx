4d71fc491e550cf263960ce065ee1a25844f09fe
Robin Thöne <>
2022-06-24 00:03:32 +0200

    add missing aria label

--- app/components/LanguageSelector.tsx
+++ app/components/LanguageSelector.tsx
@@ -22,7 +22,7 @@ export const LanguageSelector: FunctionComponent = () => {
 
     return (
         <div className="dropdown dropdown-end">
-            <button tabIndex={0} className="btn btn-circle btn-ghost mb-1">
+            <button aria-label={t('Language_Button_Aria_Label')} tabIndex={0} className="btn btn-circle btn-ghost mb-1">
                 {icon}
             </button>
             <ul tabIndex={0} className="dropdown-content menu shadow rounded-box w-40">
